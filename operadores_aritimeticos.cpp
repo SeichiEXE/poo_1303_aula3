9f74b4478d3c33ab8e675e5690947c25f78d6df5
SeichiEXE <127798960+SeichiEXE@users.noreply.github.com>
2023-03-27 16:58:47 -0300

    update

--- operadores_aritimeticos.cpp
+++ operadores_aritimeticos.cpp
@@ -1,15 +1,23 @@
+//inserindo bibliotecas
 #include <iostream>
 using namespace std;
+//inicio do codigo
 int main() {
+    //inserindo valor das variaveis num1 e num2
     int num1 = 10, num2 = 4;
+    //inserindo variaveis de operações
     int soma, multi, divi, modulo, subt;
-    
+    //inserindo operação de soma
     soma = num1 + num2;
+    //inserindo operação de multiplicação
     multi = num1 * num2;
+    //inserindo operação de divisão
     divi = num1 / num2;
+    //inserindo operação de modulo
     modulo = num1%num2;
+    //inserindo operação de subtração
     subt = num1 - num2;
-    
+    //resultado das operações
     cout<<soma<<endl;
     cout<<multi<<endl;
     cout<<divi<<endl;
