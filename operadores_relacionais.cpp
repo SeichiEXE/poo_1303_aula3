9afa146a6f8d6ec185f2beaa698a844e53b7d894
SeichiEXE <127798960+SeichiEXE@users.noreply.github.com>
2023-03-27 17:11:49 -0300

    update

--- operadores_relacionais.cpp
+++ operadores_relacionais.cpp
@@ -1,26 +1,33 @@
+//inserindo bibliotecas
 #include <iostream>
 using namespace std;
-
+//inicio do codigo
 int main() {
+    //inserindo variavel e valor de "x"
     int x = 10;
+    //inserindo variavel e valor de "y"
     int y = 20;
-    
+    //condição onde x é maior que y
     if (x > y) {
         cout << "x é maior que y" << endl;
     }
+    //condição onde x é menor que y
     if (x < y) {
         cout << "x é menor que y" << endl;
     }
-    
+    //condição onde x é maior ou igual a y
     if (x >= y) {
         cout << "x é maior ou igual a y" << endl;
     }
+    //condição onde x é menor ou igual a y
     if (x <= y) {
         cout << "x é menor ou igual a y" << endl;
     }
+    //condição onde x é igual a y
     if (x == y) {
         cout << "x é igual a y" << endl;
     }
+    //condição onde x é diferente de y
     if (x != y) {
         cout << "x é diferente de y" << endl;
     }
