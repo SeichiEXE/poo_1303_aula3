106369fa27d9cebb471f45a17b3a17f87cce24be
SeichiEXE <127798960+SeichiEXE@users.noreply.github.com>
2023-03-27 15:12:53 -0300

    Update Valor_megaman.cpp

--- Valor_megaman.cpp
+++ Valor_megaman.cpp
@@ -1,21 +1,32 @@
+//inserindo bibliotecas
 #include <iostream>
 #include <string>
 using namespace std;
-
+//Inserindo variveis usadas no personagem
 int main() {
+    //variavel de vida inserida
     int life;
+    //variavel de energia da arma inserida
     float energy;
+    //variavel de status de vida inserida (Vivo ou morto)
     bool status;
+    //variavel do nome do personagem inserida (MegaMan)
     string character;
-    
+    //Inserir valor da variavel life
     life = 9;
+    //inserir valor da variavel energy
     energy = 99.99f;
+    //inserir valor da variavel status do personagem
     status = true;
+    //inserir o nome da variavel character (nome do personagem)
     character = "Mega Man";
-    
+    //condições da variavel life
     cout<<life<<endl;
+    //condições da variavel energy
     cout<<energy<<endl;
+    //condições da variavel status
     cout<<status<<endl;
+    //condições da variavel character
     cout<<character<<endl;
     
     return 0;
